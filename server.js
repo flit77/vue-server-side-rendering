5b1436dfc8dfcb032152b503eb96139afaf5a55e
Vasiliy Shubko <flit77@gmail.com>
2017-03-23 21:36:26 +0100

    server script

---
+++ server.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const express = require('express');
+const { createBundleRenderer } = require('vue-server-renderer');
+
+const bundleRenderer = createBundleRenderer(
+  require('./dist/vue-ssr-bundle.json'),
+  {
+    template: fs.readFileSync('./index.html', 'utf-8')
+  }
+);
+
+const app = express();
+
+app.use('/dist', express.static('dist'));
+
+app.get('*', (req, res) => {
+  bundleRenderer
+    .renderToStream({url: req.path})
+    .pipe(res);
+});
+
+app.listen(8080);
